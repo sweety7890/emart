6c5f47b6e233e96f0f7e89d7b4646989c0f0e728
vollala srividya <sweety2033900@gmail.com>
2022-06-24 14:35:47 +0530

    shop

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div className="container">
           <NavLink className="navbar-brand fw-bold fs-4" to="/">
-            LA COLLECTION
+            ShopForHome
           </NavLink>
           <button
             className="navbar-toggler"
